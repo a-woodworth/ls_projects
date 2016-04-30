7613d97cf665f368fa275ab0520b9e5acbc990bb
Amy Woodworth <woodworthcommunications@gmail.com>
2016-04-30 16:43:20 -0500

    Add bonus features to calculator.

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -4,95 +4,111 @@
 # asks for the type of operation to perform: add, subtract, multiply or divide
 # displays the result
 
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+# LANGUAGE = 'en'
+LANGUAGE = 'es'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
 def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(number)
-  number.to_i != 0
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
 end
 
 def selection_to_message(sel)
-  case sel
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+  word = case sel
+          when '1'
+            prompt(messages('add', LANGUAGE))
+          when '2'
+            prompt(messages('subtract', LANGUAGE))
+          when '3'
+            prompt(messages('multiply', LANGUAGE))
+          when '4'
+            prompt(messages('divide', LANGUAGE))
+          end
+
+  x = "A random line of code"
+
+  word
 end
 
-prompt("Welcome to Calculator. Please enter your name: ")
+prompt(messages('welcome', LANGUAGE))
 
 name = ''
 loop do
   name = gets.chomp
 
   break unless name.empty?
-  prompt("Make sure to use a valid name.")
+  prompt(messages('valid_name', LANGUAGE))
 end
 
-prompt("Hi #{name}! Ready to add, subtract, multiply, or divide?")
+prompt(messages('greeting_part1', LANGUAGE) + name + messages('greeting_part2', LANGUAGE))
 
 loop do # main loop
   first_number = ''
   loop do
-    prompt("Please enter your first number.")
-    first_number = gets.chomp.to_i
+    prompt(messages('valid_first_number', LANGUAGE))
+    first_number = gets.chomp
 
-    break if valid_number?(first_number)
-    prompt("Hmmm...that doesn't look like a valid number.")
+    break if number?(first_number)
+    prompt(messages('number_error', LANGUAGE))
   end
 
   second_number = ''
   loop do
-    prompt("Please enter your second number.")
-    second_number = gets.chomp.to_i
+    prompt(messages('valid_second_number', LANGUAGE))
+    second_number = gets.chomp
 
-    break if valid_number?(second_number)
-    prompt("Hmmm...that doesn't look like a valid number.")
+    break if number?(second_number)
+    prompt(messages('number_error', LANGUAGE))
   end
 
-  selection_prompt = <<-MSG
-
-    What would you like to do? Select a number:
-      Add = 1
-      Subtract = 2
-      Multiply = 3
-      Divide = 4
-  MSG
-
-  prompt(selection_prompt)
+  prompt(messages('selection_instructions_1', LANGUAGE))
+  prompt(messages('selection_instructions_2', LANGUAGE))
+  prompt(messages('selection_instructions_3', LANGUAGE))
+  prompt(messages('selection_instructions_4', LANGUAGE))
+  prompt(messages('selection_instructions_5', LANGUAGE))
 
   selection = ''
   loop do
     selection = gets.chomp
 
     break if %w(1 2 3 4).include?(selection)
-    prompt("Must choose 1, 2, 3 or 4.")
+    prompt(messages('valid_selection', LANGUAGE))
   end
 
-  prompt("#{selection_to_message(selection)} the two numbers...")
+  prompt(selection_to_message(selection))
 
   result =  case selection
             when '1'
-              first_number + second_number
+              first_number.to_f + second_number.to_f
             when '2'
-              first_number - second_number
+              first_number.to_f - second_number.to_f
             when '3'
-              first_number * second_number
+              first_number.to_f * second_number.to_f
             when '4'
               first_number.to_f / second_number.to_f
             end
 
-  prompt("Your answer is: #{result}")
+  prompt(messages('result', LANGUAGE) + result.to_s)
 
-  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  prompt(messages('keep_calculating', LANGUAGE))
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for using the calculator. Goodbye!")
+prompt(messages('goodbye', LANGUAGE))

74e9875e457ed6492df7d62d4eaba4e41c0f79f7
Amy Woodworth <woodworthcommunications@gmail.com>
2016-04-30 10:19:31 -0500

    Refactor calulator.

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -4,28 +4,95 @@
 # asks for the type of operation to perform: add, subtract, multiply or divide
 # displays the result
 
-puts "Welcome to Calculator. Ready to add, subtract, multiply, or divide?"
-puts "-------------------------------"
-puts "Please enter your first number."
+def prompt(message)
+  puts "=> #{message}"
+end
 
-first_number = gets.chomp.to_i
-puts "Please enter your second number."
+def valid_number?(number)
+  number.to_i != 0
+end
 
-second_number = gets.chomp.to_i
-puts "-------------------------------"
+def selection_to_message(sel)
+  case sel
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
 
-puts "What would you like to do? Select a number: Add = 1, Subtract = 2, Multiply = 3, Divide = 4."
-selection = gets.chomp.to_i
+prompt("Welcome to Calculator. Please enter your name: ")
 
-result =
-  if selection == 1
-    first_number + second_number
-  elsif selection == 2
-    first_number - second_number
-  elsif selection == 3
-    first_number * second_number
-  else
-    first_number.to_f / second_number.to_f
+name = ''
+loop do
+  name = gets.chomp
+
+  break unless name.empty?
+  prompt("Make sure to use a valid name.")
+end
+
+prompt("Hi #{name}! Ready to add, subtract, multiply, or divide?")
+
+loop do # main loop
+  first_number = ''
+  loop do
+    prompt("Please enter your first number.")
+    first_number = gets.chomp.to_i
+
+    break unless valid_number?(first_number)
+    prompt("Hmmm...that doesn't look like a valid number.")
   end
 
-puts "Your answer is: #{result}"
+  second_number = ''
+  loop do
+    prompt("Please enter your second number.")
+    second_number = gets.chomp.to_i
+
+    break unless valid_number?(second_number)
+    prompt("Hmmm...that doesn't look like a valid number.")
+  end
+
+  selection_prompt = <<-MSG
+
+    What would you like to do? Select a number:
+      Add = 1
+      Subtract = 2
+      Multiply = 3
+      Divide = 4
+  MSG
+
+  prompt(selection_prompt)
+
+  selection = ''
+  loop do
+    selection = gets.chomp
+
+    break unless %w(1 2 3 4).include?(selection)
+    prompt("Must choose 1, 2, 3 or 4.")
+  end
+
+  prompt("#{selection_to_message(selection)} the two numbers...")
+
+  result =  case selection
+            when '1'
+              first_number + second_number
+            when '2'
+              first_number - second_number
+            when '3'
+              first_number * second_number
+            when '4'
+              first_number.to_f / second_number.to_f
+            end
+
+  prompt("Your answer is: #{result}")
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Goodbye!")

70b3f209cd597155c8a7c90cff460786dc9bcd2f
Amy Woodworth <woodworthcommunications@gmail.com>
2016-05-04 19:34:08 -0500

    Refactor all validate methods. Add loan formula math. Close out loop to end or run another calculation.

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -18,106 +18,93 @@ def float?(number)
 end
 
 def validate_loan(loan_amount)
-  if loan_amount == '0'
+  if loan_amount.to_f <= 0
     prompt(messages('loan_error', LANGUAGE))
-    prompt(get_loan_amount)
-  elsif loan_amount = float?(loan_amount)
-    loan_amount.to_f.to_s
+  elsif loan_amount.to_f && loan_amount.to_f > 0
+    float?(loan_amount) && loan_amount.to_f.to_s
   else # For letters/words
     prompt(messages('loan_error', LANGUAGE))
-    prompt(get_loan_amount)
   end
 end
 
 def validate_apr(apr)
-  if apr == '0'
+  if apr.to_f == 0
     prompt(messages('interest_free', LANGUAGE))
-  elsif apr = float?(apr)
-    apr.to_f.to_s
+  elsif apr.to_f > 0
+    float?(apr) && apr.to_f.to_s
   else # For letters/words
     prompt(messages('apr_error', LANGUAGE))
-    prompt(get_apr)
   end
 end
 
 def validate_years(years)
-  if years == '0'
+  if years.empty? || years.to_i < 0
     prompt(messages('years_error', LANGUAGE))
-    prompt(get_years)
-  elsif years = float?(years)
-    years.to_f.to_s
+  elsif years.to_i > 0
+    years.to_i.to_s
   else
     prompt(messages('years_error', LANGUAGE))
-    prompt(get_years)
   end
 end
 
-# Get loan amount
-def get_loan_amount
-  prompt(messages('enter_loan_amount', LANGUAGE))
-  loan_amount = gets.chomp
-  loan_amount.delete!('$,') # If added, remove $ and , from value
-  if validate_loan(loan_amount) != nil
-    prompt(messages('value_confirmation', LANGUAGE) +
-      sprintf('$%.2f', loan_amount))
-  end
+def clear_screen
+  system('clear') || system('cls')
 end
 
-# Get Annual Percentage Rate
-def get_apr
-  prompt(messages('enter_apr', LANGUAGE))
-  apr = gets.chomp
-  apr.delete!('%') # If added, remove % from value
-  if validate_apr(apr) != nil
-    prompt(messages('value_confirmation', LANGUAGE) + apr + '%.')
-    prompt(messages('monthly_rate', LANGUAGE) + monthly_rate(apr).to_s + '%.')
-  end
-end
-
-# Monthly interest rate
-def monthly_rate(apr)
-  apr = apr.to_f
-  rate = apr / 1200
-  rate.round(6)
-end
-
-# Get loan duration in years
-def get_years
-  prompt(messages('enter_years', LANGUAGE))
-  years = gets.chomp
-  if validate_years(years) != nil
-    prompt(messages('value_confirmation', LANGUAGE) + years + ' years.')
-  end
-end
-
-# Loan duration in months
-def number_of_monthly_payments
-end
-
-def calculate_payment
+prompt(messages('welcome', LANGUAGE))
+prompt(messages('usage', LANGUAGE))
+loop do
   loan_amount = ''
   apr = ''
-  monthly_rate = 0
   years = ''
 
-  prompt(messages('welcome', LANGUAGE))
-  prompt(messages('usage', LANGUAGE))
-
   loop do # Ask user for loan amount
-    prompt(get_loan_amount)
-    break unless loan_amount.to_f > 0
+    prompt(messages('enter_loan_amount', LANGUAGE))
+    loan_amount = gets.chomp
+    loan_amount.delete!('$,') # If added, remove $ and , from value
+    validate_loan(loan_amount)
+    break if loan_amount.to_f > 0
   end
+  prompt(messages('value_confirmation', LANGUAGE) +
+    format('$%.2f', loan_amount))
 
-  loop do # Ask user for APR
-    prompt(get_apr)
+  loop do # Ask user for Annual Percentage Rate
+    prompt(messages('enter_apr', LANGUAGE))
+    apr = gets.chomp
+    apr.delete!('%') # If added, remove % from value
+    validate_apr(apr)
     break if apr.to_f >= 0
   end
+  prompt(messages('value_confirmation', LANGUAGE) + apr + '%.')
 
   prompt(messages('years_greeting', LANGUAGE))
   loop do # Ask user for number of years (loan duration)
-    prompt(get_years)
-    break unless years.to_f > 0
+    prompt(messages('enter_years', LANGUAGE))
+    years = gets.chomp
+    validate_years(years)
+    break if years.to_i > 0
+  end
+  prompt(messages('value_confirmation', LANGUAGE) + years + ' years.')
+
+  annual_rate = apr.to_f / 100
+  monthly_rate = annual_rate / 12
+  months = years.to_i * 12
+
+  calculated_payment =  (monthly_rate * loan_amount.to_f) /
+                        (1 - ((1 + monthly_rate)**-months))
+
+  prompt(messages('payment', LANGUAGE) + format('$%.2f', calculated_payment))
+
+  prompt(messages('instructions', LANGUAGE))
+  answer = gets.chomp
+  if answer.downcase.start_with?('q')
+    break
+  elsif answer.downcase.start_with?('y')
+    prompt(messages('new_calculation', LANGUAGE))
+    clear_screen
+  else
+    prompt(messages('instructions_help', LANGUAGE))
+    break
   end
 end
-
-calculate_payment
+prompt(messages('goodbye', LANGUAGE))

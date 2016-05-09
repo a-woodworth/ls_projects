65818dfffdf7561c0e3a382355fe46fd389ecf38
Amy Woodworth <woodworthcommunications@gmail.com>
2016-05-09 17:30:48 -0500

    Start to refactor game to add more features.

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,19 +1,35 @@
 # Build a Rock Paper Scissors game. The game flow should go like this: the user makes a choice,
 # the computer makes a choice, the winner is displayed.
-VALUE_CHOICES = %w(rock paper scissors)
 
-def test_method
-  prompt('test message')
-end
+VALUE_CHOICES = %w(rock paper scissors lizard spock)
+
+BEATS = {
+  'rock' => %w(scissors lizard s l),
+  'paper' => %w(rock spock p sp),
+  'scissors' => %w(paper lizard p l),
+  'lizard' => %w(spock paper sp l),
+  'spock' => %w(scissors rock s r)
+}
+
+# classic_message = 'r = rock, p = paper, s = scissors.'
+# lizard_spock_message = 'r = rock, p = paper, s = scissors, l = lizard, sp = spock.'
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def win?(first, second)
-  first == 'rock' && second == 'scissors' ||
-    first == 'paper' && second == 'rock' ||
-    first == 'scissors' && second == 'paper'
+  BEATS[first].include?(second)
+  # first == 'rock' && second == 'scissors' ||
+  #   first == 'rock' && second == 'lizard' ||
+  #   first == 'paper' && second == 'rock' ||
+  #   first == 'paper' && second == 'spock' ||
+  #   first == 'scissors' && second == 'paper' ||
+  #   first == 'scissors' && second == 'lizard' ||
+  #   first == 'lizard' && second == 'spock' ||
+  #   first == 'lizard' && second == 'paper' ||
+  #   first == 'spock' && second == 'scissors' ||
+  #   first == 'spock' && second == 'rock'
 end
 
 def display_results(player, computer)

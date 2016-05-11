3caf3bf9cd699bbde103a47936c960a90c4d25b9
Amy Woodworth <woodworthcommunications@gmail.com>
2016-05-11 16:04:04 -0500

    Add game choice option. Start work on keeping score.

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,14 +1,15 @@
 # Build a Rock Paper Scissors game. The game flow should go like this: the user makes a choice,
 # the computer makes a choice, the winner is displayed.
 
-VALUE_CHOICES = %w(rock paper scissors lizard spock)
+CLASSIC_GAME = %w(rock paper scissors)
+LIZARD_SPOCK_GAME = %w(rock paper scissors lizard spock)
 
 BEATS = {
-  'rock' => %w(scissors lizard s l),
-  'paper' => %w(rock spock p sp),
-  'scissors' => %w(paper lizard p l),
-  'lizard' => %w(spock paper sp l),
-  'spock' => %w(scissors rock s r)
+  'rock' => %w(scissors lizard),
+  'paper' => %w(rock spock),
+  'scissors' => %w(paper lizard),
+  'lizard' => %w(spock paper),
+  'spock' => %w(scissors rock)
 }
 
 # classic_message = 'r = rock, p = paper, s = scissors.'
@@ -18,18 +19,29 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def game_selection(game)
+  if game.to_i == 1
+    prompt("You're playing: #{CLASSIC_GAME.join(', ')}.")
+  elsif game.to_i == 2
+    prompt("You're playing: #{LIZARD_SPOCK_GAME.join(', ')}.")
+  else
+    prompt("Sorry, Ultra only has two games. Choose 1 or 2 to play.")
+  end
+end
+
 def win?(first, second)
   BEATS[first].include?(second)
-  # first == 'rock' && second == 'scissors' ||
-  #   first == 'rock' && second == 'lizard' ||
-  #   first == 'paper' && second == 'rock' ||
-  #   first == 'paper' && second == 'spock' ||
-  #   first == 'scissors' && second == 'paper' ||
-  #   first == 'scissors' && second == 'lizard' ||
-  #   first == 'lizard' && second == 'spock' ||
-  #   first == 'lizard' && second == 'paper' ||
-  #   first == 'spock' && second == 'scissors' ||
-  #   first == 'spock' && second == 'rock'
+end
+
+def score(player, computer)
+  if win?(player, computer)
+    player_score += 1
+  elsif win?(computer, player)
+    computer_score += 1
+  else
+    player_score += 0
+    computer_score += 0
+  end
 end
 
 def display_results(player, computer)
@@ -42,26 +54,38 @@ def display_results(player, computer)
   end
 end
 
+prompt("Welcome to Rock, Paper, Scissors Ultra Addition.")
+
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALUE_CHOICES.join(', ')}")
-    choice = gets.chomp
-
-    if VALUE_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
-  end
-
-  computer_choice = VALUE_CHOICES.sample
-  prompt("You chose: #{choice}. The computer chose: #{computer_choice}.")
-
-  display_results(choice, computer_choice)
-
-  prompt("Do you want to play again?")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt("Play the classic game? Select 1 to start.")
+  prompt("Play the Spock-lizard game? Select 2.")
+  game = gets.chomp
+  game_selection(game)
+  break if game.to_i == 1 || game.to_i == 2
 end
+
+# loop do
+#   choice = ''
+#   player_score = 0
+#   computer_score = 0
+#   loop do
+#     prompt("Choose one: #{VALUE_CHOICES.join(', ')}")
+#     choice = gets.chomp
+
+#     if VALUE_CHOICES.include?(choice)
+#       break
+#     else
+#       prompt("That's not a valid choice.")
+#     end
+#   end
+
+#   computer_choice = VALUE_CHOICES.sample
+#   prompt("You chose: #{choice}. The computer chose: #{computer_choice}.")
+
+#   display_results(choice, computer_choice)
+
+#   prompt("Do you want to play again?")
+#   answer = gets.chomp
+#   break unless answer.downcase.start_with?('y')
+# end
 prompt("Thanks for playing! Goodbye.")

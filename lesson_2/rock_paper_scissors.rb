b9bebe886023befc6199e5b3b5cd9a2bdbf73773
Amy Woodworth <woodworthcommunications@gmail.com>
2016-05-12 10:03:16 -0500

    Add game selection option. Need to improve error handling for choice loop.

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -12,37 +12,48 @@ BEATS = {
   'spock' => %w(scissors rock)
 }
 
-# classic_message = 'r = rock, p = paper, s = scissors.'
-# lizard_spock_message = 'r = rock, p = paper, s = scissors, l = lizard, sp = spock.'
-
 def prompt(message)
   puts "=> #{message}"
 end
 
-def game_selection(game)
-  if game.to_i == 1
-    prompt("You're playing: #{CLASSIC_GAME.join(', ')}.")
-  elsif game.to_i == 2
-    prompt("You're playing: #{LIZARD_SPOCK_GAME.join(', ')}.")
-  else
-    prompt("Sorry, Ultra only has two games. Choose 1 or 2 to play.")
+def choose_game(game)
+  if game == '1'
+    game = CLASSIC_GAME
+    prompt("You're playing: #{game.join(', ')}.")
+    prompt("Choose one: r = rock, p = paper, s = scissors.")
+  elsif game == '2'
+    game = LIZARD_SPOCK_GAME
+    prompt("You're playing: #{game.join(', ')}.")
+    prompt("Choose one: r = rock, p = paper, s = scissors,
+       l = lizard, k = spock.")
   end
+  game
 end
 
 def win?(first, second)
   BEATS[first].include?(second)
 end
 
-def score(player, computer)
-  if win?(player, computer)
-    player_score += 1
-  elsif win?(computer, player)
-    computer_score += 1
-  else
-    player_score += 0
-    computer_score += 0
+def player_picks(item)
+  case item
+    when 'r' then 'rock'
+    when 'p' then 'paper'
+    when 's' then 'scissors'
+    when 'l' then 'lizard'
+    when 'k' then 'spock'
   end
 end
+
+# def score(player, computer)
+#   if win?(player, computer)
+#     player_score += 1
+#   elsif win?(computer, player)
+#     computer_score += 1
+#   else
+#     player_score += 0
+#     computer_score += 0
+#   end
+# end
 
 def display_results(player, computer)
   if win?(player, computer)
@@ -54,38 +65,52 @@ def display_results(player, computer)
   end
 end
 
+def clear_screen
+  system('clear') || system('cls')
+end
+
 prompt("Welcome to Rock, Paper, Scissors Ultra Addition.")
 
 loop do
+  choice = ''
   prompt("Play the classic game? Select 1 to start.")
   prompt("Play the Spock-lizard game? Select 2.")
   game = gets.chomp
-  game_selection(game)
-  break if game.to_i == 1 || game.to_i == 2
+
+  loop do
+    if game == '1' || game == '2'
+      game = choose_game(game)
+    else
+      prompt("Sorry, Ultra only has two games. Choose 1 or 2 to play.")
+      game = gets.chomp
+      game = choose_game(game)
+    end
+
+    choice = gets.chomp.downcase
+    choice = player_picks(choice)
+
+    if game.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = game.sample
+  prompt("You chose: #{choice}. The computer chose: #{computer_choice}.")
+
+  display_results(choice, computer_choice)
+
+  prompt("Select --> Y to play again. Q to quit.")
+  answer = gets.chomp
+  if answer.downcase.start_with?('q')
+    break
+  elsif answer.downcase.start_with?('y')
+    prompt("OK...starting a new game.")
+    clear_screen
+  else
+    prompt("Hmmm...I'm not sure what to do.")
+    break
+  end
 end
-
-# loop do
-#   choice = ''
-#   player_score = 0
-#   computer_score = 0
-#   loop do
-#     prompt("Choose one: #{VALUE_CHOICES.join(', ')}")
-#     choice = gets.chomp
-
-#     if VALUE_CHOICES.include?(choice)
-#       break
-#     else
-#       prompt("That's not a valid choice.")
-#     end
-#   end
-
-#   computer_choice = VALUE_CHOICES.sample
-#   prompt("You chose: #{choice}. The computer chose: #{computer_choice}.")
-
-#   display_results(choice, computer_choice)
-
-#   prompt("Do you want to play again?")
-#   answer = gets.chomp
-#   break unless answer.downcase.start_with?('y')
-# end
 prompt("Thanks for playing! Goodbye.")

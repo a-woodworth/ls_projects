23cee85bcc873a504638bdd925b920574f53c248
Amy Woodworth <woodworthcommunications@gmail.com>
2016-05-21 18:19:09 -0500

    Fix score calculation and loop prompts. Improve game instructions.

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,8 +1,25 @@
 # Build a Rock Paper Scissors game. The game flow should go like this: the user makes a choice,
 # the computer makes a choice, the winner is displayed.
 
-CLASSIC_GAME = %w(rock paper scissors)
-LIZARD_SPOCK_GAME = %w(rock paper scissors lizard spock)
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+INSTRUCTIONS = "Choose one: r = rock, p = paper, s = scissors,
+       l = lizard, k = spock."
+
+RULES = <<-MSG
+RULES OF THE GAME:
+---------------------
+* Scissors cuts Paper
+* Paper covers Rock
+* Rock crushes Lizard
+* Lizard poisons Spock
+* Spock smashes Scissors
+* Scissors decapitates Lizard
+* Lizard eats Paper
+* Paper disproves Spock
+* Spock vaporizes Rock
+* Rock crushes Scissors
+MSG
 
 BEATS = {
   'rock' => %w(scissors lizard),
@@ -12,56 +29,33 @@ BEATS = {
   'spock' => %w(scissors rock)
 }
 
+WINNING_SCORE = 5
+
 def prompt(message)
   puts "=> #{message}"
-end
-
-def choose_game(game)
-  if game == '1'
-    game = CLASSIC_GAME
-    prompt("You're playing: #{game.join(', ')}.")
-    prompt("Choose one: r = rock, p = paper, s = scissors.")
-  elsif game == '2'
-    game = LIZARD_SPOCK_GAME
-    prompt("You're playing: #{game.join(', ')}.")
-    prompt("Choose one: r = rock, p = paper, s = scissors,
-       l = lizard, k = spock.")
-  end
-  game
 end
 
 def win?(first, second)
   BEATS[first].include?(second)
 end
 
-def player_picks(item)
-  case item
-    when 'r' then 'rock'
-    when 'p' then 'paper'
-    when 's' then 'scissors'
-    when 'l' then 'lizard'
-    when 'k' then 'spock'
+def player_picks(letter)
+  case letter
+  when 'r' then 'rock'
+  when 'p' then 'paper'
+  when 's' then 'scissors'
+  when 'l' then 'lizard'
+  when 'k' then 'spock'
   end
 end
 
-# def score(player, computer)
-#   if win?(player, computer)
-#     player_score += 1
-#   elsif win?(computer, player)
-#     computer_score += 1
-#   else
-#     player_score += 0
-#     computer_score += 0
-#   end
-# end
-
 def display_results(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    prompt("You won a point.")
   elsif win?(computer, player)
-    prompt("Computer won!")
+    prompt("Computer won a point.")
   else
-    prompt("It's a tie!")
+    prompt("It's a tie! No points given.")
   end
 end
 
@@ -69,45 +63,86 @@ def clear_screen
   system('clear') || system('cls')
 end
 
-prompt("Welcome to Rock, Paper, Scissors Ultra Addition.")
+prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock Game.")
+prompt('')
+prompt(RULES)
+prompt('')
+prompt("------------------------------------------------")
+prompt("The winner of each round gets a point.")
+prompt("#{WINNING_SCORE} points wins the game! Let's play!")
+prompt("------------------------------------------------")
+prompt('')
 
 loop do
-  choice = ''
-  prompt("Play the classic game? Select 1 to start.")
-  prompt("Play the Spock-lizard game? Select 2.")
-  game = gets.chomp
+  player_score = 0
+  computer_score = 0
+  prompt(INSTRUCTIONS)
 
   loop do
-    if game == '1' || game == '2'
-      game = choose_game(game)
-    else
-      prompt("Sorry, Ultra only has two games. Choose 1 or 2 to play.")
-      game = gets.chomp
-      game = choose_game(game)
+    choice = ''
+
+    loop do
+      choice = gets.chomp.downcase
+      choice = player_picks(choice)
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
 
-    choice = gets.chomp.downcase
-    choice = player_picks(choice)
+    clear_screen
+    computer_choice = VALID_CHOICES.sample
+    prompt("You chose: #{choice}. The computer chose: #{computer_choice}.")
 
-    if game.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
+    display_results(choice, computer_choice)
+
+    if win?(choice, computer_choice)
+      player_score += 1
+    elsif win?(computer_choice, choice)
+      computer_score += 1
     end
+
+    prompt('')
+    prompt("------------------------------------------------")
+    prompt('')
+    prompt("Your score: #{player_score}.")
+    prompt("The computer's score: #{computer_score}.")
+    prompt('')
+    prompt("------------------------------------------------")
+    prompt('')
+    prompt(INSTRUCTIONS)
+
+    break if  player_score >= WINNING_SCORE ||
+              computer_score >= WINNING_SCORE
   end
 
-  computer_choice = game.sample
-  prompt("You chose: #{choice}. The computer chose: #{computer_choice}.")
-
-  display_results(choice, computer_choice)
+  if player_score == WINNING_SCORE
+    prompt('')
+    prompt("------------------------------------------------")
+    prompt("|                                              |")
+    prompt("|             YOU WON THE GAME :)              |")
+    prompt("|                                              |")
+    prompt("------------------------------------------------")
+    prompt('')
+  elsif computer_score == WINNING_SCORE
+    prompt('')
+    prompt("------------------------------------------------")
+    prompt("|                                              |")
+    prompt("|             YOU LOST THE GAME :(             |")
+    prompt("|                                              |")
+    prompt("------------------------------------------------")
+    prompt('')
+  end
 
   prompt("Select --> Y to play again. Q to quit.")
   answer = gets.chomp
   if answer.downcase.start_with?('q')
     break
   elsif answer.downcase.start_with?('y')
-    prompt("OK...starting a new game.")
     clear_screen
+    prompt("OK...starting a new game.")
   else
     prompt("Hmmm...I'm not sure what to do.")
     break

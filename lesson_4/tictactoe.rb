7b39ce63181c9810818992da768f6cc80649d08a
Amy Woodworth <woodworthcommunications@gmail.com>
2016-05-27 15:42:57 -0500

    Modify code based on rubocop suggestions.

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -1,8 +1,12 @@
 # Make a Tic Tac Toe game.
 
-INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
 def prompt(message)
   puts "=> #{message}"
@@ -12,6 +16,7 @@ def clear_screen
   system('clear') || system('cls')
 end
 
+# rubocop:disable AbcSize
 def display_board(brd)
   clear_screen
   puts "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -29,6 +34,7 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable AbcSize
 
 def initialize_board
   new_board = {}
@@ -65,19 +71,11 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
-
-  winning_lines.each do |line|
-    if  brd[line[0]] == PLAYER_MARKER &&
-        brd[line[1]] == PLAYER_MARKER &&
-        brd[line[2]] == PLAYER_MARKER
-        return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-        brd[line[1]] == COMPUTER_MARKER &&
-        brd[line[2]] == COMPUTER_MARKER
-        return 'Computer'
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
     end
   end
   nil
